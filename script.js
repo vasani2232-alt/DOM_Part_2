15f5ed128c8c1b2baa3c696c2ccc8c3e09bd94b3
vasani2232-alt <vasani2232@.in>
2026-02-04 19:16:53 +0530

    Part_2 OF DOM Complete

---
+++ script.js
@@ -0,0 +1,42 @@
+// let div = document.querySelector("div");
+// console.log(div);
+
+// let id = div.getAttribute("id");
+// console.log(id);
+
+// let name = div.getAttribute("name");
+// console.log(name);
+
+// let para = document.querySelector("p");
+// console.log(para.getAttribute("class","newClass"));
+
+// let div = document.querySelector("div");
+
+// div.style.backgroundColor = "purple";
+// div.style.backgroundColor = "Black";
+// // div.style.visibility = "hidden";
+
+// div.style.fontSize = "25px";
+
+// div.innerText = "Hello!"; //change the inner text
+
+// let newBtn = document.createElement("button");
+// newBtn.innerText = "Click Me!";
+
+// console.log(newBtn);
+
+// let p = document.querySelector("p");
+// // div.append(newBtn); //Adds at the end of the node
+// //div.prepend(newBtn); //Adds at the start of the node
+// //div.before(newBtn); //Adds before the node (outside)
+// p.after(newBtn); //Adds after the node (outside)
+
+let newHeading = document.createElement("h1");
+newHeading.innerHTML = "<i>Hi, I am new</i>";
+
+document.querySelector("body").prepend(newHeading);
+
+let para = document.querySelector("p");
+para.remove();
+
+newHeading.remove();
